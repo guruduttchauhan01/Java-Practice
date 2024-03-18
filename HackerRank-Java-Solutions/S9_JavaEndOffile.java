8e0566bce6d38aeb69d4816281a868cb706bf69c
Guru Dutt Chauhan <guruduttchauhan01@gmail.com>
2024-03-18 11:29:14 +0530

    Added Welcome To Java

---
+++ HackerRank-Java-Solutions/S9_JavaEndOffile.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution9 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner input = new Scanner(System.in);
+        int i = 1;
+        while(input.hasNext()){ //until EOF
+            String a = input.nextLine();
+            System.out.println(i + " " + a);
+            i++;
+        }
+    }
+}
