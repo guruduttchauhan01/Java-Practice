fb608a0aa418b6f42cc9175d5bcf82df50873463
Guru Dutt Chauhan <guruduttchauhan01@gmail.com>
2024-03-18 11:46:42 +0530

    Added Ex1: Welcome To Java

---
+++ HackerRank-Java-Solutions/S1_WelcomeToJava.java
@@ -0,0 +1,8 @@
+
+public class S1_WelcomeToJava {
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
