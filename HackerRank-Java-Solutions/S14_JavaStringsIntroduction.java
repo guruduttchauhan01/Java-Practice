8fe4454649600d2581fccbb8d9c99f50b389bb29
Guru Dutt Chauhan <guruduttchauhan01@gmail.com>
2024-03-21 10:13:52 +0530

    Added Ex14: Java Strings Introduction

---
+++ HackerRank-Java-Solutions/S14_JavaStringsIntroduction.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class S14_JavaStringsIntroduction {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println(A.length() + B.length());
+        int result = A.compareTo(B);
+        if(result > 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1) + " " + B.substring(0, 1).toUpperCase() + B.substring(1));
+
+    }
+}
+
+
+
