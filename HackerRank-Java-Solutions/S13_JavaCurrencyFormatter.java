309cbaf0ee607dd78a8d57d8a0582788a20c81d7
Guru Dutt Chauhan <guruduttchauhan01@gmail.com>
2024-03-21 10:13:25 +0530

    Added Ex13: Java Currency Formatter

---
+++ HackerRank-Java-Solutions/S13_JavaCurrencyFormatter.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class S13_JavaCurrencyFormatter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+
+        System.out.println("US: " + NumberFormat.getCurrencyInstance(Locale.US).format(payment));
+        System.out.println("India: " + NumberFormat.getCurrencyInstance(new Locale("en", "IN")).format(payment));
+        System.out.println("China: " + NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment));
+        System.out.println("France: " + NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment));
+    }
+}
